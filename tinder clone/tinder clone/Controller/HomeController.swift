795b145ba4f9bc8c30e9385697bf41e58af0b0cc
Marc Hulterström <marc.hulterstrom@gmail.com>
2021-02-04 09:55:56 +0100

    Setting up project and changing root viewcontroller

---
+++ tinder clone/tinder clone/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeController.swift
+//  tinder clone
+//
+//  Created by marchelmon on 2021-02-04.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
